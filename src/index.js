8a2515ed308e7e5c2a618c0be7f9f320983306d4
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2020-06-25 17:56:55 -0500

    fixed moue panning

--- src/index.js
+++ src/index.js
@@ -23,19 +23,23 @@ const sketch = (p) => {
 			p.push();
 			p.translate(this.pos.x, this.pos.y, this.pos.z);
   		//   p.rotateX(this.size,0,0);
-  		  	p.box(this.size);
+			p.box(this.size);
 			p.fill(this.color);
-  		  	// p.fill(255,0,0,this.trig);
-  		  	p.box(this.size * 1.5);
-  		  
-			if(this.trig == 255) {
-				if(this.canTrig) {
-					let rnd = p.floor(p.random(10));
-					synths[ rnd ].envelope.attack = this.r/1000;
-					synths[ rnd ].triggerAttackRelease( Tone.Midi( ((255/20 + 2) - this.size + 2) * 10 ).toFrequency(), this.size/500);
-					this.canTrig = false;
-				}
-			}
+			// p.fill(255,0,0,this.trig);
+			p.box(this.size * 1.5);
+		
+			// let rnd = p.floor(p.random(10));
+			// synths[ rnd ].envelope.attack = 1/1000;
+			// synths[ rnd ].triggerAttackRelease( Tone.Midi( ((255/20 + 2) - this.size + 2) * 10 ).toFrequency(), this.size/500);
+	
+			// if(this.trig == 255) {
+			// 	if(this.canTrig) {
+			// 		let rnd = p.floor(p.random(10));
+			// 		synths[ rnd ].envelope.attack = this.r/1000;
+			// 		synths[ rnd ].triggerAttackRelease( Tone.Midi( ((255/20 + 2) - this.size + 2) * 10 ).toFrequency(), this.size/500);
+			// 		// this.canTrig = false;
+			// 	}
+			// }
 
 
   		  this.trig-=100;
@@ -78,7 +82,6 @@ const sketch = (p) => {
 	let synths = [];
 
 	var player;
-
 	var last_index = 0;
 
 	var tsne_data = null;
@@ -86,6 +89,7 @@ const sketch = (p) => {
 	var current_date = null;
 
 	let color_palette = ['rgb(166,206,227)','rgb(31,120,180)','rgb(178,223,138)','rgb(51,160,44)','rgb(251,154,153)','rgb(227,26,28)','rgb(253,191,111)','rgb(255,127,0)','rgb(202,178,214)','rgb(106,61,154)']
+
 
 	moment.locale('es');
 
@@ -134,99 +138,39 @@ const sketch = (p) => {
 
 	p.setup = () => {
 
-		let canvas = p.createCanvas(p.windowWidth-100,p.windowHeight-20, p.WEBGL);
+		let canvas = p.createCanvas(p.windowWidth-20,p.windowHeight-50, p.WEBGL);
 		p.smooth();
 
 		data_length = Object.keys(tsne_data).length;
 
 		current_date = moment(tsne_data[100].date);
+		// const counter = selectAll('.counter');
 
-		// console.log(date.format('MM-DD-YYYY HH:mm'))
-  		// for(let i=0; i<data_length; i++) {			
-			
-		// 	if ( current_date.format('MM-DD-YYYY HH') == moment(tsne_data[i].date).format('MM-DD-YYYY HH') ) {
-
-		// 		// getting data from tsne
-		// 		let f1 = tsne_data[i].tsne_coords_1*10;
-		// 		let f2 = tsne_data[i].tsne_coords_2*10;
-		// 		let f3 = tsne_data[i].tsne_coords_3*10;
-
-		// 		// let f1 = p.random(255);
-		// 		// let f2 = p.random(255);
-		// 		// let f3 = p.random(255);
-		
-		// 		// add generated data to an array of feature vectors
-		// 		features.push( [f1, f2, f3] ); // feature vector's length (components) can be selected freely
-			
-		// 		let d = new DataPoint();
-		// 		// use data as color components for the datapoints
-		// 		// d.setCol( f1, f2, f3, 255 ).setSize(f1/20 + 2);
-		// 		// d.setCol( 255, 255, tsne_data[i].topic_num*25.5, 255 ).setSize(f1/20 + 2);
-		// 		d.setCol( color_palette[tsne_data[i].topic_num] ).setSize(1);
-		// 		data.push(d);	
-		// 	}
-
-		// }
-  		
-		// Y = features;
 	}
 
 
 	let zp = 0;
 	p.draw = () => {
 
-		let targetX = p.constrain(p.mouseX/p.width * 10 + p.sin(-p.frameCount/200) * 200,0,p.width);
+		let targetX = p.map(p.mouseX,0,p.width,-10,10);
 		let dx = targetX - x;
 		x += dx * easing;
 
-		let targetY = p.constrain(p.mouseY/p.height * 10  + p.cos(-p.frameCount/200) * 200,0,p.height);
+		let targetY = p.map(p.mouseY,0,p.height,-10,10);
 		let dy = targetY - y;
 		y += dy * easing;
 
-		p.camera(p.sin(p.frameCount/300) * 10+x, p.cos(p.frameCount/300) * 10+y, (p.cos(p.frameCount/600)/8+1)*175, 0, 0, 0, 0, 1, 0);
-		p.background(0);
+		p.camera(p.sin(p.frameCount/300) * 100+x, p.cos(p.frameCount/300) * 200+y, (p.cos(p.frameCount/600)/8+1)*175, 0, 0, 0, 0, 1, 0);
+		p.background(150);
+		p.clear();
 		p.frameRate(60);
 		
-		playHeadx = p.map(x,0,p.width,-50,50);
-		playHeady = p.map(y,0,p.height,-50,50);
-		
-		/*if(playHeadx > 250) {
-			playHeadx = -250;
-			for(let i=0; i< Y.length; i++) {
-    			data[i].canTrig = true;
-    		}
-		}*/
 
 		panner.pan.value = playHeadx / 250;
 
 		p.pointLight(150, 150, 150, p.frameCount%p.width, 0, 200);
 		// p.directionalLight(255,255,255, -1, 0, -1);
 		p.ambientLight(0);
-
-		//p.rotateY(p.sin(p.frameCount/1000) * p.PI/4);
-		//p.rotateX(-p.frameCount/1000);
-
-		// zp = p.sin(p.frameCount/100) * 250;
-		// p.push();
-		// p.translate(playHeadx,0,0);
-		// p.fill(255,120);
-		// p.noStroke();
-		// p.box(1,500,0);
-		// p.pop();
-
-		// p.push();
-		// p.translate(0,playHeady,0);
-		// p.fill(255,120);
-		// p.noStroke();
-		// p.box(500,1,0);
-		// p.pop();
-
-		// p.push();
-		// p.translate(playHeadx,playHeady,0);
-		// p.fill(255,0,0,40);
-		// p.noStroke();
-		// p.box(1,1,500);
-		// p.pop();
 
 		stepCount++;
   		if(stepCount<600) {
@@ -246,13 +190,14 @@ const sketch = (p) => {
 				let d = new DataPoint();
 
 				d.setCol( color_palette[tsne_data[i].topic_num] ).setSize(1);
-				d.setPos( p.createVector(x_coord, y_coord, z_coord) ).drawPoint();
 				
-				// if(p.dist(data[i].pos.x,data[i].pos.y,0, playHeadx,playHeady, 0) <= 100) {
-				// 	data[i].trig = 255;
-				// } else {
-				// 	data[i].canTrig = true;
-				// }
+				if( p.random() < 0.5 ) {
+					d.trig = 255;
+					d.canTrig = true;
+				}
+
+				d.setPos( p.createVector(x_coord, y_coord, z_coord) ).drawPoint();
+
 
 			} else if (moment(tsne_data[i].date).isBefore(an_hour_before)) {
 				last_index = i;
